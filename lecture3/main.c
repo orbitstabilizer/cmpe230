42774adafcd34c51e081274dcb752f4d2c6182c7
orbitstabilizer <71184281+orbitstabilizer@users.noreply.github.com>
2023-02-28 09:55:23 +0300

    enums

--- lecture3/main.c
+++ lecture3/main.c
@@ -1,9 +1,18 @@
 #include <stdio.h> // preprocesser statement
+/*
+#define YOUNG 1
+#define MIDDLE 2
+#define OLD 3
+*/
+enum age {young, middle, old}; //enums
 
+enum age a = young; //enum variable
 
 int func(int a);
 int N = 3; 
 int g; // global variable / variable definition
+unsigned int ui = 0; // unsigned int
+
 
 char c; // character uses 8 bits
 
@@ -11,16 +20,21 @@ int main(int argc, char *argv[]){ // command line arguments
    
     int x = N; // lacal variable
     int i; // variable declaration
-
+    float y; // floating point number
     x = func(6);
 
     x = c; // c is cast to an int
     c = 'A'; // c contains the ASCII value of 'A'  (65) == 1000001
-    
+   
+    // sizeof(int) works also
     printf("Size of char: %d:%5c:%-5x\n", sizeof(c), c, c);
                         // %c : character
                         // %5c : character with 5 spaces
                         // %-5c : character with 5 spaces on the left
+    // format specifiers
+    // %d : decimal 
+    // %x : hex
+    // %o : octail
     return 0;
 }
 
